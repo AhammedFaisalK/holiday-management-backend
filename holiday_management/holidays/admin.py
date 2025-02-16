ed1e3841b6e5a2b8bc95797d1ee481ebb9ac3f86
Ahammed Faisal <ahammedfaisal@tegain.com>
2025-02-16 13:40:11 +0530

    Updated admin.py

--- holiday_management/holidays/admin.py
+++ holiday_management/holidays/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import HolidayCache
 
-# Register your models here.
+@admin.register(HolidayCache)
+class HolidayCacheAdmin(admin.ModelAdmin):
+    list_display = ('country_code', 'year', 'created_at', 'updated_at')
+    search_fields = ('country_code', 'year')
+    list_filter = ('year', 'country_code')
+    readonly_fields = ('created_at', 'updated_at')
